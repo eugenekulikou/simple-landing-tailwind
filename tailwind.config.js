6707a78fa10e0922b9fe11a96285efe28c555461
“Eugene” <evgenij.kulikov@gmail.com>
2023-02-17 15:35:40 +0100

    Adds hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
     content: ["./src/**/*.html"],
     theme: {
-        extend: {},
+        screen: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px"
+        },
+        extend: {
+            colors: {
+                "strong-cyan": 'hsl(171, 66%, 44%)',
+                "light-blue": 'hsl(233, 100%, 69%)',
+                "grayish-blue": 'hsl(201, 11%, 66%)',
+                "dark-grayish-blue": 'hsl(210, 10%, 33%)',
+            },
+            fontFamily: {
+                sans: ['Bai Jamjuree', 'sans-serif'],
+            },
+        },
     },
     variants: {
         extend: {},
